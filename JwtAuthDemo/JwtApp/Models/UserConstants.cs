80a087eb14105654b68a0a631500c1d0d3ae4a70
alex-toy <alexei.80@hotmail.fr>
2023-01-21 08:04:09 +0100

    jwt auth demo

---
+++ JwtAuthDemo/JwtApp/Models/UserConstants.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "alex", EmailAddress = "alex.admin@email.com", Password = "MyPass_w0rd", GivenName = "Jason", Surname = "Bryant", Role = "Administrator" },
+            new UserModel() { Username = "seb", EmailAddress = "seb@email.com", Password = "MyPass_w0rd", GivenName = "Elyse", Surname = "Lambert", Role = "Seller" },
+        };
+    }
+}
--- JwtAuthDemo/JwtApp/Startup.cs
+++ JwtAuthDemo/JwtApp/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace JwtApp
 {
@@ -38,6 +33,8 @@ namespace JwtApp
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
                 };
             });
+            services.AddMvc();
+            services.AddControllers();
             services.AddRazorPages();
         }
 
